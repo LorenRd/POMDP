f73815b9cffe63fc914812bf4f9a438421f23e9c
[JoseManuelMR] <[jmmaestrerodriguez@hotmail.com]>
2019-06-26 20:57:34 +0200

    Tigre y LaserTag funcionando. Se tiene en cuenta budget y max_play

--- pomdp_runner.py
+++ pomdp_runner.py
@@ -6,6 +6,7 @@ from parsers import PomdpParser, GraphViz
 from logger import Logger as log
 import numpy as math
 
+
 class PomdpRunner:
 
     def __init__(self, params):
@@ -14,23 +15,12 @@ class PomdpRunner:
             log.new(params.logfile)
 
     def create_model(self, env_configs):
-        """
-        Builder method for creating model (i,e, agent's environment) instance
-        :param env_configs: the complete encapsulation of environment's dynamics
-        :return: concrete model
-        """
         MODELS = {
             'RockSample': RockSampleModel,
         }
         return MODELS.get(env_configs['model_name'], Model)(env_configs)
 
     def create_solver(self, algo, model):
-        """
-        Builder method for creating solver instance
-        :param algo: algorithm name
-        :param model: model instance, e.g, TigerModel or RockSampleModel
-        :return: concrete solver
-        """
         SOLVERS = {
             'pbvi': PBVI,
             'pomcp': POMCP,
@@ -41,7 +31,7 @@ class PomdpRunner:
         visualiser.update(tree.root)
         visualiser.render('./dev/snapshots/{}'.format(filename))  # TODO: parametrise the dev folder path
 
-    def run(self, modo, algo, T, **kwargs):
+    def run(self, modo, problema, algo, T, **kwargs):
         steps = math.array([], float)
         rewards = math.array([], float)
         if modo == "Benchmark":
@@ -54,13 +44,10 @@ class PomdpRunner:
             params, pomdp = self.params, None
             total_rewards, budget = 0, params.budget
 
-            #log.info('~~~ initialising ~~~')
             with PomdpParser(params.env_config) as ctx:
-                # creates model and solver
                 model = self.create_model(ctx.copy_env())
                 pomdp = self.create_solver(algo, model)
 
-                # supply additional algo params
                 belief = ctx.random_beliefs() if params.random_prior else ctx.generate_beliefs()
 
                 if algo == 'pbvi':
@@ -69,7 +56,6 @@ class PomdpRunner:
                 elif algo == 'pomcp':
                     pomdp.add_configs(budget, belief, **kwargs)
 
-            # have fun!
             if modo != "Benchmark":
                 log.info('''
                 ++++++++++++++++++++++
@@ -79,23 +65,26 @@ class PomdpRunner:
                     Horizonte de tiempo: {}
                     Numero de juegos maximo: {}
                 ++++++++++++++++++++++'''.format(model.curr_state, budget, belief, T, params.max_play))
-
-            for i in range(params.max_play):
-                # plan, take action and receive environment feedbacks
+            condicion_parada = False
+            i = 0
+            while not condicion_parada and params.max_play > i:
+                i += 1
                 pomdp.solve(T, modo)
                 action = pomdp.get_action(belief)
                 new_state, obs, reward, cost = pomdp.take_action(action)
 
+                if problema == "Tigre":
+                    condicion_parada = action == "open-left" or action == "open-right"
+                elif problema == "LaserTag":
+                    condicion_parada = action == "Catch"
+
                 if params.snapshot and isinstance(pomdp, POMCP):
-                    # takes snapshot of belief tree before it gets updated
                     self.snapshot_tree(visualiser, pomdp.tree, '{}.gv'.format(i))
 
-                # update states
                 belief = pomdp.update_belief(belief, action, obs)
                 total_rewards += reward
                 budget -= cost
 
-                # print ino
                 if modo == "Iterativo":
                     log.info('\n'.join([
                       'Accion tomada: {}'.format(action),
@@ -109,17 +98,21 @@ class PomdpRunner:
                     ]))
 
                 if budget <= 0:
-                    log.info('Budget spent.')
-            log.info('{} juegos. Recompensa total acumulada = {}'.format(i , total_rewards))
+                    log.info('Se ha sobrepasado el presupuesto establecido.')
+                if params.max_play != 'inf' and params.max_play <= i:
+                    log.info('Se ha sobrepasado el número máximo de pasos establecido.')
+
+            log.info('{} pasos ejecutados. Recompensa total acumulada = {}'.format(i, total_rewards))
             steps = math.append(steps, i)
             rewards = math.append(rewards, total_rewards)
+
         if modo == "Benchmark":
             mean_steps = steps.mean()
             std_steps = steps.std()
             mean_rewards = rewards.mean()
-            std_rewards = rewards.mean()
+            std_rewards = rewards.std()
             print("Valor medio pasos: ", mean_steps)
             print("Desviacion tipica pasos: ", std_steps)
             print("Valor medio recompensas: ", mean_rewards)
-            print("Desviacion tipica pasos: ", std_rewards)
+            print("Desviacion tipica recompensas: ", std_rewards)
         return pomdp
--- principal.py
+++ principal.py
@@ -7,7 +7,6 @@ from util import RunnerParams
 
 
 if __name__ == '__main__':
-    print("POMDP por Lorenzo y Jose Manuel")
     fin = False
     while not fin:
         print("Seleccione un problema:")
@@ -15,39 +14,51 @@ if __name__ == '__main__':
         print("[2] - LaserTag")
         print("[3] - Problema1")
         print("[4] - Problema2")
-        opcionesProblema = [1, 2, 3, 4]
+        opciones_problema = [1, 2, 3, 4]
 
-        numeroCorrecto = False
+        numero_correcto = False
         problema = ""
         while problema == "":
-            numProblema = int(input())
-            if numProblema in opcionesProblema:
-                if numProblema == 1:
+            num_problema = int(input())
+            if num_problema in opciones_problema:
+                if num_problema == 1:
                     problema = "Tigre"
                     print("Problema seleccionado:", problema)
-                elif numProblema == 2:
+                elif num_problema == 2:
                     problema = "LaserTag"
+                    budget = float('inf')
                     print("Problema seleccionado:", problema)
-                elif numProblema == 3:
+                    num_intentos = float('inf')
+                elif num_problema == 3:
                     problema = "Problema 1"
                     print("Problema seleccionado:", problema)
                 else:
                     problema = "Problema 2"
                     print("Problema seleccionado:", problema)
+
+                print("Maximo numero de intentos: ", end="")
+                max_play = input()
+                if max_play == "":
+                    max_play = float('inf')
+                if problema == "Tigre":
+                    print("Maximo presupuesto: ", end="")
+                    budget = input()
+                    if budget == "":
+                        budget = float('inf')
             else:
                 print("El número seleccionado no corresponde a ningún problema.")
 
         print("Seleccione un algoritmo resolvedor:")
         print("[1] - POMCP")
         print("[2] - PVBI")
-        opcionesAlgoritmo = ["pomcp", "pvbi"]
+        opciones_algoritmo = ["pomcp", "pvbi"]
         algoritmo = ""
         while algoritmo == "":
-            numAlgoritmo = int(input())
-            if numAlgoritmo == 1:
+            num_algoritmo = int(input())
+            if num_algoritmo == 1:
                 algoritmo = "pomcp"
                 print("Algoritmo seleccionado:", algoritmo)
-            elif numAlgoritmo == 2:
+            elif num_algoritmo == 2:
                 algoritmo = "pbvi"
                 print("Algoritmo seleccionado:", algoritmo)
             else:
@@ -57,7 +68,7 @@ if __name__ == '__main__':
         print("[1] - Iterativo")
         print("[2] - Silencioso")
         print("[3] - Benchmark")
-        opcionesModo = [1, 2, 3]
+        opciones_modo = [1, 2, 3]
         modo = ""
         while modo == "":
             numModo = int(input())
@@ -74,22 +85,14 @@ if __name__ == '__main__':
                 print("El número introducido no corresponde a ningún modo de ejecución.")
         fin = True
 
-    """
-    Parse generic params for the POMDP runner, and configurations for the chosen algorithm.
-    Algorithm configurations the JSON files in ./configs
-
-    Example usage:
-        > python main.py pomcp --env Tiger-2D.POMDP
-        > python main.py pbvi --env Tiger-2D.POMDP
-    """
-    parser = argparse.ArgumentParser(description='Solve pomdp')
-    parser.add_argument('--config', type=str, default=algoritmo, help='The file name of algorithm configuration (without JSON extension)') #pbvi o pomcp
-    parser.add_argument('--env', type=str, default=problema+".POMDP", help='The name of environment\'s config file')#.pomdp
-    parser.add_argument('--budget', type=float, default=float('inf'), help='The total action budget (default to inf)')
-    parser.add_argument('--snapshot', type=bool, default=False, help='Whether to snapshot the belief tree after each episode')
-    parser.add_argument('--logfile', type=str, default=None, help='Logfile path')
-    parser.add_argument('--random_prior', type=bool, default=False, help='Whether or not to use a randomly generated distribution as prior belief, default to False')
-    parser.add_argument('--max_play', type=int, default=10, help='Maximum number of play steps')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, default=algoritmo)
+    parser.add_argument('--env', type=str, default=problema + ".POMDP")
+    parser.add_argument('--budget', type=float, default=budget)
+    parser.add_argument('--snapshot', type=bool, default=False)
+    parser.add_argument('--logfile', type=str, default=None)
+    parser.add_argument('--random_prior', type=bool, default=False)
+    parser.add_argument('--max_play', type=int, default=max_play)
 
     args = vars(parser.parse_args())
     params = RunnerParams(**args)
@@ -98,4 +101,4 @@ if __name__ == '__main__':
         algo_params = json.load(algo_config)
         runner = PomdpRunner(params)
 
-        runner.run(modo, **algo_params)
+        runner.run(modo, problema, **algo_params)
